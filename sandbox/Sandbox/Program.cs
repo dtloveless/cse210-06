b6f85c0ebf5b28cf74db4312c22381ca190a73c3
dtloveless <80668695+dtloveless@users.noreply.github.com>
2023-01-10 14:54:46 -0700

    In class coding

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine(addNumbers(2, 2));
+    }
+
+    static int addNumbers(int num1, int num2) 
+    {
+        return num1 + num2;
     }
 }
